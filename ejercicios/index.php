5ad530031574e6979542432f454c35e68325eb76
alxrgdriguez <sralexred@gmail.com>
2024-09-25 18:35:06 +0200

    Sintaxis php

--- ejercicios/index.php
+++ ejercicios/index.php
@@ -4,16 +4,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Introducion a PHP</title>
 </head>
 
 <body>
 
     <h1>Servidor con PHP</h1>
 
+    <?php
+
+    //Ejemplo comentario una linea
+    /*Comentario
+    mas de una linea*/
+
+    echo "Hola Mundo, ESTOY EN PHP <br>";
+
+    // Declaracion de variables
+
+    $precio = 5.6;
+
+    echo "<br> El precio es " . $precio;
+
+    $precio = "Cadena"; // La variable es una cadena
+
+    echo "<br> El precio es " . $precio;
+
+    ?>
+
+    <?= "<br>Esto es más rapido aún" ?>
+
+
+
+    <p>Fin de la pagina</p>
+
 </body>
 
 </html>
-
-
-<?php
